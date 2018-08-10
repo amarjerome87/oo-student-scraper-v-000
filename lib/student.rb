fee029acb75aebfa91d5a698ec38287fe0e1fe67
Amar Chatman <amarjerome@gmail.com>
2018-08-10 14:06:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,10 +21,10 @@ class Student
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |attribute,value|
       self.send("#{attribute}=", value)
-    end 
+    end
   end
 
   def self.all
-
+    @@all
   end
 end
